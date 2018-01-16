0a1daee254d5d0484ccb13fa917412e50b270553
LY16JAVA-wangxinkai <34933871+LY16JAVA-wangxinkai@users.noreply.github.com>
2018-01-16 21:19:01 +0800

    Add files via upload

---
+++ S3阶段/Oracle/T3/上机作业/37王新凯/odbc/src/bean/Dept.java
@@ -0,0 +1,32 @@
+package bean;
+
+public class Dept {
+private int did;
+private String dname;
+private int tel;
+private String ress;
+public int getDid() {
+	return did;
+}
+public void setDid(int did) {
+	this.did = did;
+}
+public String getDname() {
+	return dname;
+}
+public void setDname(String dname) {
+	this.dname = dname;
+}
+public int getTel() {
+	return tel;
+}
+public void setTel(int tel) {
+	this.tel = tel;
+}
+public String getRess() {
+	return ress;
+}
+public void setRess(String ress) {
+	this.ress = ress;
+}
+}
