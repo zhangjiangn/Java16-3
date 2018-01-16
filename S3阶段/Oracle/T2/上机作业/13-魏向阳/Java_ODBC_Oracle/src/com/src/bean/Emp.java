2795864bc144b288154334d5319d73cffb139a5f
流风 <35030491+LY16Java-WeiXiangYang@users.noreply.github.com>
2018-01-16 13:29:43 +0800

    Add files via upload
    这是我第二章的上机作业，请接收！

---
+++ S3阶段/Oracle/T2/上机作业/13-魏向阳/Java_ODBC_Oracle/src/com/src/bean/Emp.java
@@ -0,0 +1,50 @@
+package com.src.bean;
+
+public class Emp {
+	private int empno;
+	private String ename;
+	private String job;
+	private int sal;
+	private int comm;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public int getComm() {
+		return comm;
+	}
+
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+
+}
