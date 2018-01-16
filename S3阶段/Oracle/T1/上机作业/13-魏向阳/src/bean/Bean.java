d34bc813965ff594d555b345f19d44b95971faa1
流风 <35030491+LY16Java-WeiXiangYang@users.noreply.github.com>
2018-01-16 13:18:46 +0800

    Add files via upload
    我是魏向阳，这个才是我的上机作业！那个只是和老师用来演示的...

---
+++ S3阶段/Oracle/T1/上机作业/13-魏向阳/src/bean/Bean.java
@@ -0,0 +1,44 @@
+package bean;
+
+public class Bean {
+
+	private int did ;
+	private String name ;
+	private String age ;
+	private String ress;
+	private String space;
+	
+	
+	
+	public int getDid() {
+		return did;
+	}
+	public void setDid(int did) {
+		this.did = did;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getRess() {
+		return ress;
+	}
+	public void setRess(String ress) {
+		this.ress = ress;
+	}
+	public String getSpace() {
+		return space;
+	}
+	public void setSpace(String space) {
+		this.space = space;
+	}
+	
+}
